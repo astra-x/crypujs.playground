bfd1f22fd3449a55758e6883bc79d58d327059c4
abnernat <zhang951005@gmail.com>
2020-08-07 19:25:27 +0800

    (refactor) beautify vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -6,9 +6,9 @@ module.exports = {
         secure: false, // 如果是https接口，需要配置这个参数
         changeOrigin: true, //是否跨域
         pathRewrite: {
-          "^/tests": "", //需要rewrite的,
-        },
-      },
-    },
-  },
+          "^/tests": "" //需要rewrite的,
+        }
+      }
+    }
+  }
 };
