d2c2d6d77f302988d873f7a230d68a7b3aca3b35
abnernat <zhang951005@gmail.com>
2020-08-07 19:24:21 +0800

    (feat) touch constant and view files

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  devServer: {
+    proxy: {
+      "/fisco": {
+        target: "http://47.56.165.246:8545", // 接口域名
+        secure: false, // 如果是https接口，需要配置这个参数
+        changeOrigin: true, //是否跨域
+        pathRewrite: {
+          "^/tests": "", //需要rewrite的,
+        },
+      },
+    },
+  },
+};
